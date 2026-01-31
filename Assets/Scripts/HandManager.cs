343b5ca2ad453fa29221a90c7d9cf25fff49cb81
AxRyt <xaviercaouette@gmail.com>
2026-01-31 17:52:11 -0500

    Add deck building
    Cards showing in your hands are now from a pool of scriptable objects

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
@@ -8,17 +7,21 @@ public class HandManager : MonoBehaviour
 {
     public static HandManager Instance { get; private set; }
 
+    [Header("Hand Settings")]
     [SerializeField] private int maxHandSize = 5;
-    [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private SplineContainer splineContainer;
-    [SerializeField] private Transform spawnPoint;
     [SerializeField] private int drawNumber = 3;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private SplineContainer splineContainer;
+
+    [Header("Card Settings")]
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private List<CardData> availableCards;
 
     private List<GameObject> handCards = new();
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        // Singleton pattern
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -29,6 +32,7 @@ public class HandManager : MonoBehaviour
 
     private void Start()
     {
+        // Draw initial hand only if it's already player turn
         TryDrawInitialHand();
     }
 
@@ -45,47 +49,6 @@ public class HandManager : MonoBehaviour
         }
     }
 
-    public void RemoveCard(GameObject card)
-    {
-        if (!handCards.Contains(card)) return;
-
-        handCards.Remove(card);
-        UpdateCardPoisitions();
-    }
-
-    private void DrawCard()
-    {
-        if (handCards.Count >= maxHandSize) return;
-        GameObject g = Instantiate(cardPrefab, spawnPoint.position, spawnPoint.rotation);
-
-        CardView view = g.GetComponent<CardView>();
-        view.SetHandManager(this);
-        handCards.Add(g);
-        UpdateCardPoisitions();
-    }
-    private void UpdateCardPoisitions()
-    {
-        if(handCards.Count == 0) return;
-        float cardSpacing = 1f / maxHandSize;
-        float firstCardPostion = 0.5f - (handCards.Count - 1) * cardSpacing / 2;
-        Spline spline = splineContainer.Spline;
-
-        for(int i = 0; i < handCards.Count; i++)
-        {
-            float p = firstCardPostion + i * cardSpacing;
-            Vector3 splinePosition =
-    splineContainer.transform.TransformPoint(
-        spline.EvaluatePosition(p)
-    );
-            Vector3 forward = spline.EvaluateTangent(p);
-            Vector3 up = spline.EvaluateUpVector(p);
-            Quaternion rotation = Quaternion.LookRotation(up, Vector3.Cross(up,forward).normalized);
-            handCards[i].transform.DOMove(splinePosition, 0.25f);
-            handCards[i].transform.DORotateQuaternion(rotation, 0.25f);
-        }
-
-    }
-
     private void OnEnable()
     {
         GameManager.OnPlayerTurnStarted += HandlePlayerTurnStarted;
@@ -99,23 +62,76 @@ public class HandManager : MonoBehaviour
     private void HandlePlayerTurnStarted()
     {
         Debug.Log("HandManager: Player Turn Started - Drawing Cards");
+
         for (int i = 0; i < drawNumber; i++)
         {
             DrawCard();
         }
     }
 
+    public void DrawCard()
+    {
+        if (handCards.Count >= maxHandSize) return;
+        if (availableCards.Count == 0) return;
+
+        // Pick a random card from availableCards
+        CardData data = availableCards[Random.Range(0, availableCards.Count)];
+
+        // Instantiate card prefab
+        GameObject g = Instantiate(cardPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        // Set card data on CardView
+        CardView view = g.GetComponent<CardView>();
+        if (view != null)
+            view.SetData(data);
+
+        // Add to hand and update positions
+        handCards.Add(g);
+        UpdateCardPositions();
+    }
+
+    public void RemoveCard(GameObject card)
+    {
+        if (!handCards.Contains(card)) return;
+
+        handCards.Remove(card);
+        UpdateCardPositions();
+    }
+
+    public void AddCardToDeck(CardData newCard)
+    {
+        availableCards.Add(newCard);
+    }
+
     public void ClearHand()
     {
-        // Destroy all card GameObjects
         foreach (var card in handCards)
         {
             if (card != null)
                 Destroy(card);
         }
-
-        // Clear the list
         handCards.Clear();
     }
 
+    private void UpdateCardPositions()
+    {
+        if (handCards.Count == 0) return;
+
+        float cardSpacing = 1f / maxHandSize;
+        float firstCardPosition = 0.5f - (handCards.Count - 1) * cardSpacing / 2;
+        Spline spline = splineContainer.Spline;
+
+        for (int i = 0; i < handCards.Count; i++)
+        {
+            float t = firstCardPosition + i * cardSpacing;
+
+            Vector3 splinePos = splineContainer.transform.TransformPoint(spline.EvaluatePosition(t));
+            Vector3 forward = spline.EvaluateTangent(t);
+            Vector3 up = spline.EvaluateUpVector(t);
+            Quaternion rotation = Quaternion.LookRotation(up, Vector3.Cross(up, forward).normalized);
+
+            handCards[i].transform.DOMove(splinePos, 0.25f);
+            handCards[i].transform.DORotateQuaternion(rotation, 0.25f);
+        }
+    }
 }

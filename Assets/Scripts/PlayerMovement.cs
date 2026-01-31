a27d7b0e0774c4e6c6b71db9c165e4a0ade1f6d8
AxRyt <xaviercaouette@gmail.com>
2026-01-30 22:59:52 -0500

    Change unity to both input systems

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 6f;
+
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector3(x, 0f, z).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveVelocity = moveInput * moveSpeed;
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
